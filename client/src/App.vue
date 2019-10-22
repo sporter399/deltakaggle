aa11d5104a87a600b143e48831f01e3cc50933da
sporter399 <sjporter399@gmail.com>
2019-10-21 23:47:56 -0600

    removed fodder and created title page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TitlePage title="This is DeltaKaggle"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TitlePage from './components/TitlePage.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    TitlePage
   }
 }
 </script>
---
+++ client/src/components/TitlePage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TitlePage",
+  props: ['title']
+    
+  }
+
+</script>
+
+

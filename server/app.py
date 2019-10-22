44e488085869005e3bf060a4cf109218c3f8662f
sporter399 <sjporter399@gmail.com>
2019-10-22 16:25:28 -0600

    Flask now serving vue app

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
+array = ['dogs', 'cats', 'chickens']
 
 app = Flask(__name__,
     static_folder = "./dist/static",
@@ -9,6 +10,10 @@ app = Flask(__name__,
 @app.route("/")
 def serve_vue_app():
     return (render_template('index.html'))
+
+@app.route('/array', methods = ['GET'])
+def server_array():
+    return jsonify({'animals': array})
 
 @app.after_request
 def add_header(req):

e51dd6a9beb544299cc61db38f466ac8bbf097e8
sporter399 <sjporter399@gmail.com>
2019-10-23 13:44:31 -0600

    code with difficulties at click function in Vue

--- client/src/components/TitlePage.vue
+++ client/src/components/TitlePage.vue
@@ -2,9 +2,6 @@
   <div class="hello">
     <h1>{{ title }}</h1>
     <ul>
-      <li v-for="animal in array" v-bind:key="animal"> {{ animal }}</li>
-    </ul>
-    <ul>
       <li v-for="applicant in fetched_info" v-bind:key="applicant"> {{ applicant }}</li>
     </ul>
     <input v-model="inputValue"/>
@@ -13,14 +10,15 @@
 </template>
 
 <script>
-import axios from 'axios'
+import axios from 'axios';
+
 
 export default {
   name: "TitlePage",
   props: ['title'],
   data () {
     return {
-      sql_age: [1, 2, 3],
+      vars: [1, 2, 3],
       array: ['a', 'b', 'c'],
       inputValue: ''
     }
@@ -29,16 +27,16 @@ export default {
   },
   methods: {
     userSelectsAgeClick() {
-      axios.post('/todo', { item: this.inputValue } )
-        .then(() => {
-          axios.get('/array').then( res => this.array = res.data.animals);
-        })
-      this.inputValue = '';
+      
+      
+      axios.post('/', { item: this.inputValue } )
+      
     }
   },
+  
   mounted() {
-    axios.get('/array').then( res => this.array = res.data.animals);
-      
+    
+  
      
 
   }

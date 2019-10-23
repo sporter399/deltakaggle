d37aaea66fc8e777d4814232e5ba440fc7c67fd1
sporter399 <sjporter399@gmail.com>
2019-10-22 22:41:11 -0600

    first commit with sql select with parameter

--- server/arrayAPI.py
+++ server/arrayAPI.py
@@ -22,29 +22,33 @@ trainDF.to_sql('APPLICANTS', conn, if_exists='replace', index = False)
 c.execute('''  
 SELECT * FROM APPLICANTS
           ''')
+"""
+code below is interesting:
+cursor.execute("INSERT INTO table VALUES (%s, %s, %s)", (var1, var2, var3))
+or
+cursor.execute("INSERT INTO table VALUES (?, ?, ?)", (var1, var2, var3))
+or
+t = ('RHAT',)
+c.execute('SELECT * FROM stocks WHERE symbol=?', t)
 
-sql_age = []
-c.execute("SELECT age FROM APPLICANTS;")
-result = c.fetchall()
 
-ints_only = [item[0] for item in result] 
+"""
+
+
+sql_data = []
+var = ('48',)
+c.execute("SELECT * FROM APPLICANTS WHERE age=?", var)
+fetched_info = c.fetchall()
 
 
 array = ['dogs', 'cats', 'chickens']
 
 array_api = Blueprint('array_api', __name__)
 
-for i in range(len(ints_only)):
-
-    test_var = int(ints_only[i])
-
-    if test_var < 24:
-        sql_age.append(test_var)
-
 
 @array_api.route('/array', methods=['GET'])
 def serve_array():
-    return jsonify({"animals": array}, {"applicants": sql_age})
+    return jsonify({"animals": array}, {"applicants": fetched_info})
 
 @array_api.route('/todo', methods=['POST'])
 def add_todo():
